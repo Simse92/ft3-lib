bc66a95ccd5dea6c7170f0324251668948dedf32
Simon <simon.sorgenfrei@haw-hamburg.de>
2019-10-21 12:36:45 +0200

    added some bodies to transaction

--- src/Ft3/PaymentOperationExtractor.cs
+++ src/Ft3/PaymentOperationExtractor.cs
@@ -1,1 +1,19 @@
-// ToDo
+namespace Chromia.Postchain.Ft3
+{
+    public class PaymentOperationExtractor
+    {
+        private readonly byte[] _transaction;
+        private readonly string _chainId;
+
+        public PaymentOperationExtractor(byte[] rawTransaction, string chainId)
+        {
+            this._transaction = rawTransaction;
+            this._chainId = chainId;
+        }
+
+        public PaymentOperation[] Extract()
+        {
+            // ToDO
+        }
+    }
+}
--- src/Ft3/TransactionBuilder.cs
+++ src/Ft3/TransactionBuilder.cs
@@ -2,7 +2,7 @@ namespace Chromia.Postchain.Ft3
 {
     public class TransactionBuilder
     {
-        public readonly BlockchainInfo Blockchain;
+        public readonly Blockchain Blockchain;
         public TransactionBuilder(Blockchain blockchain)
         {
             this.Blockchain = blockchain;
